447c7a6cd840da73db4311de5a040cdab9ad15d8
Njc27 <jc.n@northeastern.edu>
2024-03-18 22:37:00 -0400

    server changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
 var mongoose = require("mongoose");
 var cors = require('cors');
 
